6857c9ea2052d1e772d082574816f349bfe31837
abeidahmed <abeidahmed92@gmail.com>
2021-01-14 22:57:57 +0530

    Make sessions create action

---
+++ spec/requests/sessions_request_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Sessions", type: :request do
+  describe "#create" do
+    it "signs in the user if the request is valid" do
+      user = create(:user)
+      post sessions_path, params: { email_address: user.email_address, password: user.password }
+
+      expect(cookies[:auth_token]).to eq(user.auth_token)
+    end
+
+    it "returns an error if the request is invalid" do
+      post sessions_path, params: { email_address: "", password: "helloworld" }
+
+      expect(json.dig(:errors, :invalid)).to be_present
+    end
+  end
+end
