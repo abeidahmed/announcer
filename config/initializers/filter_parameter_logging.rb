88eb73de6280412cc1f4d4eed26895c2a210b27c
abeidahmed <abeidahmed92@gmail.com>
2021-01-14 23:14:18 +0530

    Fix rubocop errors

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,6 +1,8 @@
+# rubocop:disable all
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
   :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
 ]
+# rubocop:enable all
