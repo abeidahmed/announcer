39bcddabf606a57599506ebdf562bd1d29244aaa
abeidahmed <abeidahmed92@gmail.com>
2021-01-14 21:22:48 +0530

    Make user create action

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Users", type: :request do
+  describe "#create" do
+    context "when the request is valid" do
+      before do
+        post users_path, params: { user: attributes_for(:user).except(:auth_token) }
+      end
 
+      it "will create a new user if the request is valid" do
+        expect(User.count).to eq(1)
+      end
+
+      it "will store the auth_token in the cookie" do
+        expect(cookies[:auth_token]).to eq(User.first.auth_token)
+      end
+    end
+
+    it "returns an error if the request is invalid" do
+      post users_path, params: { user: attributes_for(:user).except(:auth_token, :email_address) }
+
+      expect(json.dig(:errors, :email_address)).to be_present
+    end
+  end
 end
