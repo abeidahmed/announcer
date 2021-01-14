54cb600e24a7f4e325f125d3a53c31a8bcc39f0e
abeidahmed <abeidahmed92@gmail.com>
2021-01-14 20:58:17 +0530

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence(:email_address) { |n| "johndoe#{n}@example.com" }
     full_name { "John Doe" }
     password { "secretpassword" }
+    auth_token { "authtoken" }
   end
 end
