e6fa72991191c68406161a9c8f60e93347aa5935
abeidahmed <abeidahmed92@gmail.com>
2021-01-14 20:56:09 +0530

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,32 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  before_validation :strip_email_address, :strip_full_name
+  before_save :lowercase_email_address
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates(
+    :email_address,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false },
+  )
+  validates :full_name, presence: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  private
+
+  def strip_email_address
+    self.email_address = email_address.to_s.strip
+  end
+
+  def strip_full_name
+    self.full_name = full_name.to_s.strip
+  end
+
+  def lowercase_email_address
+    self.email_address = email_address.to_s.downcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,47 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { build(:user) }
+
+  describe "validations" do
+    it { is_expected.to have_secure_password }
+
+    it { is_expected.to validate_presence_of(:email_address) }
+
+    it { is_expected.to validate_uniqueness_of(:email_address).case_insensitive }
+
+    it { is_expected.to validate_length_of(:email_address).is_at_most(255) }
+
+    it { is_expected.to allow_value("johndoe@example.com", "johjn@exa.co.in").for(:email_address) }
+
+    it { is_expected.not_to allow_value("johndoeexample.com", "johjn@exa").for(:email_address) }
+
+    it { is_expected.to validate_presence_of(:full_name) }
+
+    it { is_expected.to validate_length_of(:full_name).is_at_most(255) }
+
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+
+    it "lowercases email_address before saving" do
+      email_address = "hello@example.com"
+      user.email_address = email_address.upcase
+      user.save!
+
+      expect(user.email_address).to eq(email_address)
+    end
+
+    it "strips the email_address before validation" do
+      user.email_address = "   hello@example.com "
+      user.save!
+
+      expect(user.email_address).to eq("hello@example.com")
+    end
+
+    it "strips the full_name before validation" do
+      user.full_name = "  John Doe Howard "
+      user.save!
+
+      expect(user.full_name).to eq("John Doe Howard")
+    end
+  end
 end
