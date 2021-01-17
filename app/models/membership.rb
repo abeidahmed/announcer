1c19f18d1efdda436eb13ddf9172cb5db3616d2e
abeidahmed <abeidahmed92@gmail.com>
2021-01-17 21:38:53 +0530

    Add membership model validations

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,8 @@
 class Membership < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+  enum role: { editor: "editor", owner: "owner" }
+
+  validates :user_id, uniqueness: { scope: :project_id, case_sensitive: false, message: "is already on the project team" } # rubocop:disable Layout/LineLength
 end
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Membership, type: :model do
 
     it { is_expected.to belong_to(:project) }
   end
+
+  describe "validations" do
+    # rubocop:disable Layout/LineLength
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:project_id).case_insensitive.with_message("is already on the project team") }
+
+    it { is_expected.to define_enum_for(:role).with_values(editor: "editor", owner: "owner").backed_by_column_of_type(:string) }
+    # rubocop:enable Layout/LineLength
+  end
 end
