bd1a4e726bed067adc689da3197168da5b353992
abeidahmed <abeidahmed92@gmail.com>
2021-01-19 18:19:50 +0530

    Set default value for role and add invited field

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,4 +5,13 @@ class Membership < ApplicationRecord
   enum role: { editor: "editor", owner: "owner" }
 
   validates :user_id, uniqueness: { scope: :project_id, case_sensitive: false, message: "is already on the project team" } # rubocop:disable Layout/LineLength
+
+  def role=(role_type)
+    default_role = role_type.presence || "editor"
+    super(default_role)
+  end
+
+  def invited=(value)
+    self.join_date = value ? "" : Time.zone.now
+  end
 end
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -16,4 +16,34 @@ RSpec.describe Membership, type: :model do
     it { is_expected.to define_enum_for(:role).with_values(editor: "editor", owner: "owner").backed_by_column_of_type(:string) }
     # rubocop:enable Layout/LineLength
   end
+
+  describe "#role" do
+    it "sets the role to editor if role is not mentioned" do
+      membership.role = nil
+
+      expect(membership.role).to eq("editor")
+    end
+
+    it "sets the role to user defined var if role is mentioned" do
+      membership.role = "owner"
+
+      expect(membership.role).to eq("owner")
+    end
+  end
+
+  describe "#invited" do
+    it "sets the join_date to empty string if invited is set as true" do
+      membership.join_date = Time.zone.now
+      membership.invited = true
+
+      expect(membership.join_date).to be_blank
+    end
+
+    it "sets the join_date to current time if invited is set as false" do
+      membership.join_date = ""
+      membership.invited = false
+
+      expect(membership.join_date).to be_present
+    end
+  end
 end
