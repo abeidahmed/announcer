655c97fd3afae572bd95dcc39ffccdb5f50fd11b
abeidahmed <abeidahmed92@gmail.com>
2021-01-20 09:57:37 +0530

    Accept user hash instead of email_address

--- app/models/inviter.rb
+++ app/models/inviter.rb
@@ -1,16 +1,16 @@
 class Inviter
-  def initialize(project, email_address:, role: "editor", invited: false)
-    @project       = project
-    @email_address = email_address
-    @role          = role
-    @invited       = invited
+  def initialize(project, colleague:, role: "editor", invited: true)
+    @project   = project
+    @colleague = colleague
+    @role      = role
+    @invited   = invited
 
     invite
   end
 
   def user
     @user ||= User.create_with(
-      full_name: "hello",
+      full_name: full_name,
       password: SecureRandom.urlsafe_base64
     ).find_or_initialize_by(email_address: email_address)
   end
@@ -21,9 +21,17 @@ class Inviter
 
   private
 
-  attr_reader :project, :email_address, :role, :invited
+  attr_reader :project, :role, :invited, :colleague
 
   def add_colleague_to_team
     Membership.create(project: project, user: user, role: role, invited: invited)
+  end
+
+  def full_name
+    colleague[:full_name]
+  end
+
+  def email_address
+    colleague[:email_address].to_s.downcase
   end
 end
--- spec/models/inviter_spec.rb
+++ spec/models/inviter_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe Inviter, type: :model do
   let(:project) { create(:project) }
 
   describe "#initialize" do
-    let(:inviter) { Inviter.new(project, email_address: "x@tu.com") }
+    let(:inviter) { Inviter.new(project, colleague: { email_address: "x@tu.com", full_name: "hello" }) }
 
     it "sets the project" do
       expect(inviter.instance_variable_get(:@project)).to eq(project)
     end
 
-    it "sets the email_address to the described email_address" do
-      expect(inviter.instance_variable_get(:@email_address)).to eq("x@tu.com")
+    it "sets the @colleague to user hash" do
+      expect(inviter.instance_variable_get(:@colleague)).to eq({ email_address: "x@tu.com", full_name: "hello" })
     end
 
     it "sets the role to editor" do
@@ -19,22 +19,43 @@ RSpec.describe Inviter, type: :model do
     end
 
     it "sets the invited to false" do
-      expect(inviter.instance_variable_get(:@invited)).to eq(false)
+      expect(inviter.instance_variable_get(:@invited)).to eq(true)
     end
   end
 
   describe "#user" do
     it "finds the user" do
       user = create(:user)
-      inviter = Inviter.new(project, email_address: user.email_address)
+      inviter = Inviter.new(project, colleague: { email_address: user.email_address.upcase, full_name: user.full_name })
 
       expect(inviter.user).to eq(user)
     end
 
     it "creates a new user if the user is not found" do
-      inviter = Inviter.new(project, email_address: "hello@ex.com")
+      inviter = Inviter.new(project, colleague: { email_address: "hello@ex.com", full_name: "hello" })
 
       expect(User.first.email_address).to eq("hello@ex.com")
+      expect(User.first.full_name).to eq("hello")
+    end
+  end
+
+  describe "#invite" do
+    let(:user) { create(:user) }
+
+    before do
+      Inviter.new(project, colleague: { email_address: user.email_address, full_name: user.full_name })
+    end
+
+    it "adds the user to the project membership" do
+      expect(Membership.first.email_address).to eq(user.email_address)
+    end
+
+    it "adds the user as invited" do
+      expect(Membership.first.join_date).to be_blank
+    end
+
+    it "adds the user as an editor" do
+      expect(Membership.first.role).to eq("editor")
     end
   end
 end
